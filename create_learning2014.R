8ff9e8986f0284f44c9ca5b624856d2072b1bca6
madelenelindqvist <madelene.lindqvist@helsinki.fi>
2017-02-02 14:48:05 +0200

    Add files via upload

---
+++ create_learning2014.R
@@ -0,0 +1,12 @@
+#Madelene Lindqvist
+#2.2.2017
+#First RStudio data wrangling exercise
+
+learning2014<-read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt\t", header=T)
+str(learning2014)
+dim(learning2014)
+
+#The data has got 183 observations and 60 variable
+#Variables are e.g. attitude, points and gender
+
+
